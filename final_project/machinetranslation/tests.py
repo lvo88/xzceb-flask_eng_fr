1c10ddfa123a0c99839247e7cc7b7841ae68fa1d
Long Vo <long_rmit@yahoo.com>
2022-12-23 22:47:11 -0500

    final assignment

---
+++ final_project/machinetranslation/tests.py
@@ -0,0 +1,22 @@
+"""Import modules"""
+import unittest
+
+from translator import english_to_french, french_to_english
+
+
+class test_english_to_french(unittest.TestCase):
+    def test1(self):
+        #Test null input
+        #self.assertIsNone()
+        #Test Hello
+        self.assertEqual(english_to_french("Hello"), "Bonjour")
+
+class test_french_to_english(unittest.TestCase):
+    def test1(self):
+        #Test null input
+        #self.assertIsNone()
+        #Test Bonjour
+        self.assertEqual(french_to_english("Bonjour"), "Hello")
+
+if __name__=="__main__":
+    unittest.main()
